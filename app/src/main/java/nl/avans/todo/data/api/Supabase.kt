fad182c163e10f040530db9ad81acfa3af6fa7b8
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2025-07-06 14:08:51 +0200

    removed key

--- app/src/main/java/nl/avans/todo/data/api/Supabase.kt
+++ app/src/main/java/nl/avans/todo/data/api/Supabase.kt
@@ -1,6 +1,6 @@
 package nl.avans.todo.data.api
 
 object SupabaseConfig {
-    const val BASE_URL = "https://jkymaonendnipyeoxwpy.supabase.co"
-    const val API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpreW1hb25lbmRuaXB5ZW94d3B5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2ODc3MjUsImV4cCI6MjA1OTI2MzcyNX0.74XqQSLyS6ufeRo2pAHCeskQ5O1rxvDD_VVVpIlIikQ"
+    const val BASE_URL = "YOURURL"
+    const val API_KEY = "YOURKEY"
 }
